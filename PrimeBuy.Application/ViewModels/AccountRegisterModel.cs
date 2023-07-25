37754d65f0b353e68592591d54fbbef964ae9a10
antonioernesto1 <antonioernestodossantos@gmail.com>
2023-07-25 20:19:25 -0300

    changing buttons visuals and fixing product delete bug

--- PrimeBuy.Application/ViewModels/AccountRegisterModel.cs
+++ PrimeBuy.Application/ViewModels/AccountRegisterModel.cs
@@ -9,6 +9,8 @@ namespace PrimeBuy.Application.ViewModels
     {
         public string Id { get; set; }
         public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Password { get; set; }
         public string CPF { get; set; }
         public DateTime Birthdate { get; set; }
